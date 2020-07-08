63f7b00f059dceaa3208a80fdf6440187604dbd0
Alexandr Efimov <a.efimov@rusled.ru>
2020-07-08 16:50:19 +0300

    fix: исправлены ошибки после проверки

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,25 @@ let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let formElement = document.querySelector ('.popup__form');
 
-openPopup.addEventListener('click', () => {
-    popup.classList.add('popup_open');
-})
+function showClick() {
+    popup.classList.add('popup_open'); 
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
 
-closePopup.addEventListener('click', () => {
+openPopup.addEventListener('click', showClick);
+
+function popupClose() {
     popup.classList.remove('popup_open');
-})
+}
 
-formElement.addEventListener('submit', (event) => {
+closePopup.addEventListener('click', popupClose);
+
+function formSubmitHandler(event) {
     event.preventDefault();
-    
     profileName.textContent = nameInput.value; 
     profileJob.textContent = jobInput.value;
-
     popup.classList.remove('popup_open');
-})
+}
+
+formElement.addEventListener('submit', formSubmitHandler );
